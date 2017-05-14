d7cc1980744dae5bbc9182a129a039e12843749e
dnnsys <as@rsdigital.ru>
2017-05-14 18:10:25 +0300

    changed stick header

--- resources/assets/javascripts/Application.js
+++ resources/assets/javascripts/Application.js
@@ -3,13 +3,19 @@ import ReviewsBlock from './views/ReviewsBlock'
 import PartnersBlock from './views/PartnersBlock'
 import Rellax from 'rellax'
 import './vendor/jQuery.scrollSpeed'
-import Headhesive from 'headhesive'
+//import Headhesive from 'headhesive'
+import './vendor/anchor'
 
 class Application{
     constructor(){
         console.log('application start');
         document.addEventListener('DOMContentLoaded', () => {
-
+            // $(function() {
+            //     $('a[href*=\\#]').anchor({
+            //         transitionDuration : 1000,
+            //         topOffsetPosition: 200
+            //     });
+            // });
              new ReviewsBlock();
              new PartnersBlock();
              new Rellax('.rellax');
@@ -35,24 +41,31 @@ class Application{
         //     let fromTop = $("body").scrollTop();
         //     $('body').toggleClass("down", (fromTop > 200));
         // });
-        let options = {
-            offset: 200,
-            classes: {
-
-                // Cloned elem class
-                clone: 'header--clone',
-
-                // Stick class
-                stick: 'header--stick',
-
-                // Unstick class
-                unstick: 'header--unstick'
-            }
-        }
-        let header = header = new Headhesive('.header', options);
-        if(screen.width < 880){
-            header.destroy();
-        }
+        ////////////////////////////////////////////
+        // let options = {
+        //     offset: 200,
+        //     classes: {
+        //
+        //         // Cloned elem class
+        //         clone: 'header--clone',
+        //
+        //         // Stick class
+        //         stick: 'header--stick',
+        //
+        //         // Unstick class
+        //         unstick: 'header--unstick'
+        //     }
+        // }
+        // let header = header = new Headhesive('.header', options);
+        // if(screen.width < 880){
+        //     header.destroy();
+        // }
+        let $header = $('.header');
+        $(window).on("scroll", function() {
+            if ($(window).scrollTop() > 150) $header.addClass('header--stick animated slideInDown').removeClass('slideInUp');
+            //else if ($(window).scrollTop() > 150 ) $header.addClass('header--stick');
+            else $header.removeClass('header--stick slideInDown').addClass('slideInUp');
+        });
     }
 }
 
